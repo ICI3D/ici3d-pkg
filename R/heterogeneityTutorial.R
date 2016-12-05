08d92aac1636b02b9069604b2ede03a70da6307a
Carl Pearson <cap10@ufl.edu>
2016-12-05 09:10:35 -0500

    updating heterogeneity tutorial

--- R/heterogeneityTutorial.R
+++ R/heterogeneityTutorial.R
@@ -2,8 +2,9 @@
 NULL
 
 het.population <- function(
-  pop.size, beta.mean, beta.var
+  pop.size, beta.mean, beta.var, seed = 0
 ) {
+  set.seed(seed)
   theta = beta.var / beta.mean
   kk = beta.mean / theta
   mxdst <- rgamma(pop.size, shape = kk, scale = theta)
@@ -87,13 +88,13 @@ het.run <- function(runid, mxdst, end.time, gmma, rho) {
   return(ts[, runid := runid ])
 }
 
-base.het.plot <- function(end.time, maxpop) ggplot() +
+base.het.plot <- function(end.time, maxpop, dt=data.table(runid=integer(), time = numeric(), state=factor(), value=numeric())) ggplot() +
   aes(x=time, alpha=runid, color=state, y=value, group=interaction(runid, state)) +
   scale_alpha_continuous(limits = c(-10, NA)) +
   scale_x_continuous(limits = c(0, end.time)) +
   scale_y_continuous(limits = c(0, maxpop)) +
   theme_minimal() + guides(alpha="none") + theme(legend.position = "bottom", legend.direction = "horizontal") +
-  geom_line(data=data.table(runid=integer(), time = numeric(), state=factor(), value=numeric()))
+  geom_line(data=dt)
 
 
 het.epidemic.runs <- function(
@@ -128,7 +129,10 @@ het.ui <- shinyUI(fluidPage(
     tabPanel('Parts 1 & 2: Low Variance',
       fluidRow(column(12, includeMarkdown("inst/hetTut/part1.md"))),
       fluidRow(
-        column(6, numericInput("part1samples", label = "How many new runs?", value = 1, min = 1, max = 50, step = 1)), column(6, actionButton("part1click", "Run Simulations"))
+        column(3, numericInput("part1samples", label = "Add runs:", value = 1, min = 1, max = 50, step = 1)),
+        column(3, actionButton("part1click", "Run")),
+        column(3, textOutput("part1runs", inline = T)),
+        column(3, textOutput("part1TODO", inline = T))
       ),
       fluidRow(
         column(4, plotOutput("part1hist")), column(4, plotOutput("part1series")), column(4, plotOutput("part1sizes"))
@@ -137,12 +141,15 @@ het.ui <- shinyUI(fluidPage(
     ),
     tabPanel('Parts 3 & 4: Changing Variance and Population',
       fluidRow(column(12, includeMarkdown("inst/hetTut/part3.md"))),
-      fluidRow(column(6, numericInput(
-        "part3var",
-        label = "Desired variance?",
-        value=0.001, min = 0.0001, max = 1
-      )),
-      column(6, actionButton("part34click", "Run Simulations"))),
+      fluidRow(column(4, numericInput(
+          "part3var",
+          label = "Desired variance?",
+          value=0.001, min = 0.0001, max = 1
+        )),
+        column(2, actionButton("part34click", "Run")),
+        column(3, textOutput("part34runs", inline = T)),
+        column(3, textOutput("part34TODO", inline = T))
+      ),
       fluidRow(
         column(4, plotOutput("part34hist")),
         column(4, plotOutput("part34series")),
@@ -181,179 +188,166 @@ het.ui <- shinyUI(fluidPage(
 # server block needs to a maximum amount of work (aka, simulation runs)
 # then release for render updates, but resume after if there's still work to do
 
+emptyseries <- data.table(runid=integer(), time=numeric(), state=factor(levels=c("S","I","R")), value=integer())
+emptydistro <- data.table(runid=integer(), cumulativeI=integer())
+
+
 het.server <- shinyServer({
 
   part1mxdst <- het.population(pop.size = 100, beta.mean = 2, beta.var = 0.001)
 
-  allowedIterations <- 10
+  allowedIterations <- 100
 
   part34samples <- 30
 
   cycleTracking <- reactiveValues(
-    part1was = 0, part1TODO = F, part1cycles = 0,
-    part34was = 0, part34TODO = F, part34cycles = 0,
+    part1was = 0, part1cycles = 0,
+    part34was = 0, part34cycles = 0,
     part5was = 0, part5TODO = F, part5cycles = 0,
     itersLeft = allowedIterations
   )
 
   rvs <- reactiveValues(
-    part1series = base.het.plot(end.time = 10, maxpop=100),
-    part1distro = data.table(runid=integer(), cumulativeI=integer()),
+    part1series = emptyseries,
+    part1distro = emptydistro,
     part1index = 0,
     part34mxdst = het.population(pop.size = 100, beta.mean = 2, beta.var = 0.001),
-    part34distro = data.table(runid=integer(), cumulativeI=integer())
+    part34distro = emptydistro,
+    part34series = emptyseries,
+    part34index = 0
   )
 
   function(input, output, session) {
 
     observe({
-      input$part34click
-      rvs$part34mxdst <- het.population(pop.size = isolate(input$part4var), beta.mean = 2, beta.var = isolate(input$part3var))
-      rvs$part34series <- base.het.plot(end.time = 10, maxpop=isolate(input$part4var))
-      # rvs$part34series <- rvs$part34series + geom_line(data=addedts[state != "cumulativeI"])
-      #rvs$part34distro <- addedts[state=="cumulativeI", list(cumulativeI=value[.N]), by=runid]
+      input$part5click
+      rvs$part5Amxdst <- het.population(
+        pop.size = 100, beta.mean = isolate(input$part5var), beta.var = 0.1
+      )
+      rvs$part5Bmxdst <- het.population(
+        pop.size = 100, beta.mean = isolate(input$part5var), beta.var = 10
+      )
+      rvs$part5Aseries <- base.het.plot(end.time = 10, maxpop=100)
+      rvs$part5Bseries <- base.het.plot(end.time = 10, maxpop=100)
     })
 
     observe({
 
-      if (isolate(cycleTracking$part1was) != input$part1click) {
-        cycleTracking$part1TODO <- TRUE
+      if (input$part1click - isolate(cycleTracking$part1was)) {
         cycleTracking$part1cycles <- isolate(input$part1samples)
         cycleTracking$part1was <- input$part1click
-      }
-
-      if (isolate(cycleTracking$part34was) != input$part34click) {
-        cycleTracking$part34TODO <- TRUE
-        cycleTracking$part34cycles <- part34samples
-        cycleTracking$part34was <- input$part34click
-      }
-
-      if (isolate(cycleTracking$part5was) != input$part5click) {
-        cycleTracking$part5TODO <- TRUE
-        cycleTracking$part5cycles <- part34samples
-        cycleTracking$part5was <- input$part5click
       }
 
       # depend on all buttons
       # lock all buttons
 
       # do part 12 heavy lifting, if there are cycles available & there's work to do
-      isolate(if(cycleTracking$part1TODO & (cycleTracking$itersLeft > 0)) {
-        addedts <- het.epidemic.runs(part1mxdst, rvs$part1index, end.time = 10, gmma = 1)
-
-        rvs$part1series <- rvs$part1series + geom_line(data=addedts[state != "cumulativeI"])
-        rvs$part1distro <- rbind(
-          rvs$part1distro,
-          addedts[state=="cumulativeI", list(cumulativeI=value[.N]), by=runid]
-        )
-        # if done, rvs$part1TODO <- FALSE
-        rvs$part1index <- rvs$part1index + 1
-        cycleTracking$part1cycles <- cycleTracking$part1cycles - 1
-        cycleTracking$part1TODO <- cycleTracking$part1cycles != 0
-        cycleTracking$itersLeft <- cycleTracking$itersLeft - 1
+      isolate(if (cycleTracking$part1cycles) {
+          addedts <- het.epidemic.runs(part1mxdst, rvs$part1index, end.time = 10, gmma = 1)
+          rvs$part1series <- rbind(
+            rvs$part1series,
+            addedts[state != "cumulativeI"]
+          )
+          rvs$part1distro <- rbind(
+            rvs$part1distro,
+            addedts[state=="cumulativeI", list(cumulativeI=value[.N]), by=runid]
+          )
+          # if done, rvs$part1TODO <- FALSE
+          rvs$part1index <- rvs$part1index + 1
+          cycleTracking$part1cycles <- cycleTracking$part1cycles - 1
       })
 
-      isolate(if(cycleTracking$part34TODO & (cycleTracking$itersLeft > 0)) { # do part 34 heavy lifting, if there are cycles available & there's work to do
+      if (isolate(cycleTracking$part1cycles)) invalidateLater(0, session)
+    })
+
+    observe({
+      input$part34click
+      rvs$part34index <- 0
+      rvs$part34mxdst   <- het.population(pop.size = isolate(input$part4var), beta.mean = 2, beta.var = isolate(input$part3var))
+      rvs$part34series  <- emptyseries
+      rvs$part34distro  <- emptydistro
+    })
+
+    if (!isolate(cycleTracking$part1cycles)) observe({
+
+      if (input$part34click - isolate(cycleTracking$part34was)) {
+        cycleTracking$part34cycles <- part34samples
+        cycleTracking$part34was <- input$part34click
+      }
+
+      isolate(if(cycleTracking$part34cycles) { # do part 34 heavy lifting, if there are cycles available & there's work to do
 
         addedts <- het.epidemic.runs(rvs$part34mxdst, cycleTracking$part34cycles, end.time = 10, gmma = 1)
 
-        rvs$part34series <- rvs$part34series + geom_line(data=addedts[state != "cumulativeI"])
+        rvs$part34series <- rbind(
+          rvs$part34series,
+          addedts[state != "cumulativeI"]
+        )
+
         rvs$part34distro <- rbind(
           rvs$part34distro,
           addedts[state=="cumulativeI", list(cumulativeI=value[.N]), by=runid]
         )
 
+        rvs$part34index <- rvs$part34index + 1
         cycleTracking$part34cycles <- cycleTracking$part34cycles - 1
-        cycleTracking$part34TODO <- cycleTracking$part34cycles != 0
-        cycleTracking$itersLeft <- cycleTracking$itersLeft - 1
       })
 
-      isolate(if(cycleTracking$part5TODO & (cycleTracking$itersLeft > 0)) { # do part 5 heavy lifting, if there are cycles available & there's work to do
+      if (cycleTracking$part34cycles) invalidateLater(0, session)
 
-        cycleTracking$part5cycles <- cycleTracking$part5cycles - 1
-        cycleTracking$part5TODO <- cycleTracking$part5cycles != 0
-        cycleTracking$itersLeft <- cycleTracking$itersLeft - 1
-      })
-
-      output$part1TODO <- renderText(isolate(cycleTracking$part1TODO))
-      output$part1cycles <- renderText(isolate(cycleTracking$part1cycles))
-      output$itersLeft <- renderText(isolate(cycleTracking$itersLeft))
-
-      output$part1hist <- renderPlot(het.hist(part1mxdst, beta.mean = 2))
-      output$part1series <- renderPlot({ rvs$part1series })
-      output$part1sizes <- renderPlot({
-        if(rvs$part1distro[,.N != 0]) het.runs.hist(rvs$part1distro)
-      })
-
-      output$part34hist <- renderPlot(het.hist(rvs$part34mxdst, beta.mean = 2))
-      output$part34series <- renderPlot({ rvs$part34series })
-      output$part34sizes <- renderPlot({
-        if(rvs$part34distro[,.N != 0]) het.runs.hist(rvs$part34distro)
-      })
-
-      cycleTracking$itersLeft <- allowedIterations
-      if (
-        cycleTracking$part1cycles +
-        cycleTracking$part34cycles +
-        cycleTracking$part5cycles
-      ) invalidateLater(0, session)
     })
 
-    # part 1
     # observe({
-    #   if (input$part1click) {
-    #     freezeReactiveValue(input, "part1click")
-    #     progress <- shiny::Progress$new()
-    #     on.exit(progress$close())
-    #     cycleTracking$part1work <- isolate(input$part1samples)
-    #     while(isolate(cycleTracking$part1work)) {
-    #       cycleTracking$part1work
-    #     }
-    #   } else if (isolate(cycleTracking$part1work)) {
     #
+    #   if (isolate(cycleTracking$part5was) != input$part5click) {
+    #     cycleTracking$part5TODO <- TRUE
+    #     cycleTracking$part5cycles <- part34samples
+    #     cycleTracking$part5was <- input$part5click
     #   }
     #
+    #   isolate(if(cycleTracking$part5TODO & (cycleTracking$itersLeft > 0)) { # do part 5 heavy lifting, if there are cycles available & there's work to do
     #
-    #   start <- isolate(rvs$part1index) + 1
-    #   end <- start - 1 + isolate(input$part1samples)
-    #   rvs$part1index <- end
-    #   progress$inc(0, detail=sprintf("starting %d samples...", isolate(input$part1samples)))
-    #   addedts <- het.epidemic.runs(part1mxdst, start:end, end.time = 10, gmma = 1, tell.run = progress)
-    #
-    #   rvs$part1series <- isolate(rvs$part1series) + geom_line(data=addedts[state != "cumulativeI"])
-    #   rvs$part1distro <- rbind(
-    #     isolate(rvs$part1distro),
-    #     addedts[state=="cumulativeI", list(cumulativeI=value[.N]), by=runid]
-    #   )
-    # })
-    #
-    # # part 34 obs
-    # part34samps <- 30
-    # observe({
-    #   progress <- shiny::Progress$new()
-    #   on.exit(progress$close())
-    #   clicks <- input$part34click
-    #   freezeReactiveValue(input, "part34click")
-    #   progress$inc(0, detail=sprintf("starting %d samples...", part34samps))
-    #   rvs$part34mxdst <- het.population(pop.size = isolate(input$part4var), beta.mean = 2, beta.var = isolate(input$part3var))
-    #
-    #   addedts <- het.epidemic.runs(rvs$part34mxdst, 1:part34samps, end.time = 10, gmma = 1, tell.run = progress)
-    #
-    #   rvs$part34series <- base.het.plot(end.time = 10, maxpop=isolate(input$part4var)) + geom_line(data=addedts[state != "cumulativeI"])
-    #   rvs$part34distro <- addedts[state=="cumulativeI", list(cumulativeI=value[.N]), by=runid]
+    #     cycleTracking$part5cycles <- cycleTracking$part5cycles - 1
+    #     cycleTracking$part5TODO <- cycleTracking$part5cycles != 0
+    #     cycleTracking$itersLeft <- cycleTracking$itersLeft - 1
+    #   })
     #
     # })
-    #
-    # output$part1hist <- renderPlot(het.hist(part1mxdst, beta.mean = 2))
-    # output$part1series <- renderPlot({ rvs$part1series })
-    # output$part1sizes <- renderPlot({
-    #   if(rvs$part1distro[,.N != 0]) het.runs.hist(rvs$part1distro)
+
+#    cycleTracking$itersLeft <- allowedIterations
+
+#    print(sprintf("updating iters left: %d", isolate(cycleTracking$itersLeft)))
+    #    output$part1cycles <- renderText(isolate(cycleTracking$part1cycles))
+    #    output$itersLeft <- renderText(isolate(cycleTracking$itersLeft))
+
+
+    output$part1TODO <- renderText(ifelse(cycleTracking$part1cycles, "Running", "Waiting"))
+    output$part1runs <- renderText(sprintf("Total Runs: %d", rvs$part1index))
+
+    output$part1hist <- renderPlot(het.hist(part1mxdst, beta.mean = 2))
+    output$part1series <- renderPlot(base.het.plot(10, 100, dt=rvs$part1series))
+    output$part1sizes <- renderPlot({
+      if(rvs$part1distro[,.N != 0]) het.runs.hist(rvs$part1distro)
+    })
+
+    output$part34TODO <- renderText(ifelse(cycleTracking$part34cycles, "Running", "Waiting"))
+    output$part34runs <- renderText(sprintf("Total Runs: %d", rvs$part34index))
+
+    output$part34hist <- renderPlot(het.hist(rvs$part34mxdst, beta.mean = 2))
+    output$part34series <- renderPlot(base.het.plot(10, isolate(input$part4var), dt=rvs$part34series))
+    output$part34sizes <- renderPlot({
+      if(rvs$part34distro[,.N != 0]) het.runs.hist(rvs$part34distro)
+    })
+
+    # output$part5Ahist <- renderPlot(het.hist(rvs$part5Amxdst, beta.mean = input$part5var))
+    # output$part5Aseries <- renderPlot({ rvs$part5Aseries })
+    # output$part5Asizes <- renderPlot({
+    #   if(rvs$part5Adistro[,.N != 0]) het.runs.hist(rvs$part5Adistro)
     # })
-    #
-    # output$part34hist <- renderPlot(het.hist(rvs$part34mxdst, beta.mean = 2))
-    # output$part34series <- renderPlot({ rvs$part34series })
-    # output$part34sizes <- renderPlot({
-    #   if(rvs$part34distro[,.N != 0]) het.runs.hist(rvs$part34distro)
+    # output$part5Bhist <- renderPlot(het.hist(rvs$part5Bmxdst, beta.mean = input$part5var))
+    # output$part5Bseries <- renderPlot({ rvs$part5Bseries })
+    # output$part5Bsizes <- renderPlot({
+    #   if(rvs$part5Bdistro[,.N != 0]) het.runs.hist(rvs$part5Bdistro)
     # })
 
   }})
